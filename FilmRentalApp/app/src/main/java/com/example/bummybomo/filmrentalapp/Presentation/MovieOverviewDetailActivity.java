536e0f7f63eb64ae9e7d0f6d32c8b39ce7bb19e5
omidleet <omid-noor@hotmail.com>
2017-06-20 20:03:06 +0200

    overviewdetail fix

--- FilmRentalApp/app/src/main/java/com/example/bummybomo/filmrentalapp/Presentation/MovieOverviewDetailActivity.java
+++ FilmRentalApp/app/src/main/java/com/example/bummybomo/filmrentalapp/Presentation/MovieOverviewDetailActivity.java
@@ -1,14 +1,25 @@
 package com.example.bummybomo.filmrentalapp.Presentation;
 
+import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.bummybomo.filmrentalapp.Domain.Film;
 import com.example.bummybomo.filmrentalapp.R;
 
 public class MovieOverviewDetailActivity extends AppCompatActivity {
+    private Film film;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_overview_detail);
+        film = (Film) getIntent().getSerializableExtra(MovieOverview.EXTRA_MOVIE);
+
+        TextView textView = (TextView) findViewById(R.id.description_film);
+        textView.setText(film.getDescription());
     }
+
+
 }
