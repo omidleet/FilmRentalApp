c04fb321d21adea1a70fe4e8b2ab1c1f8fe52d6d
omidleet <omid-noor@hotmail.com>
2017-06-20 12:05:26 +0200

    apiconnecter get + authorization added

--- FilmRentalApp/app/src/main/java/com/example/bummybomo/filmrentalapp/Presentation/MovieOverviewDetailActivity.java
+++ FilmRentalApp/app/src/main/java/com/example/bummybomo/filmrentalapp/Presentation/MovieOverviewDetailActivity.java
@@ -1,8 +1,25 @@
 package com.example.bummybomo.filmrentalapp.Presentation;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.bummybomo.filmrentalapp.Domain.Film;
+import com.example.bummybomo.filmrentalapp.R;
+import java.util.ArrayList;
+
 /**
  * Created by bummybomo on 18-6-2017.
  */
 
-class MovieOverviewDetailActivity {
+class MovieOverviewDetailActivity extends AppCompatActivity {
+
+    private ArrayList<Film> movieList = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_overview);
+    }
+
+
 }
